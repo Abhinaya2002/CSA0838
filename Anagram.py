5622e96c215ac13dd7ec9ae3fad78b01843ce749
Abhishek200511 <112488157+Abhishek200511@users.noreply.github.com>
2022-09-14 11:05:58 +0530

    Add files via upload

---
+++ Anagram.py
@@ -0,0 +1,24 @@
+class Solve():
+   def Anagrams( self, li ):
+       '''
+       Function to Group Anagrams
+       :param li: list of words
+       :return: list of grouped anagrams
+       '''
+       dictionary = {}
+       for word in li:
+           sortedWord = ''.join(sorted(word))
+           # If word is not in dictionary
+           if sortedWord not in dictionary:
+               dictionary[sortedWord] = [word]
+           # If previously it is present that means its anagram
+           # was previously present
+           else:
+               dictionary[sortedWord] += [word]
+       return [dictionary[i] for i in dictionary]
+
+if __name__ == '__main__':
+   li = [12345]
+   # Call to function to Solve and group Anagrams
+   print(Solve().Anagrams(li))
+ 
